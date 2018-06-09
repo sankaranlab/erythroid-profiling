6a7314572e78b36bc7f1a4ccb2f361f8423f6ddd
caleblareau <caleblareau@g.harvard.edu>
2018-06-08 23:37:12 -0400

    DESeq

---
+++ code/rna/02_DESeq2.R
@@ -0,0 +1,53 @@
+library(dplyr)
+library(DESeq2)
+library(BiocParallel)
+library(IHW)
+register(MulticoreParam(2))
+
+raw <- read.table("../../processed/RNAseq_rawGeneCounts.tsv", header = TRUE)
+RNA.counts <- raw[,1:28]
+meta <- stringr::str_split_fixed(colnames(RNA.counts), "_", 4)
+
+# RNA DEseq2 setup
+RNA.counts.df <- as.data.frame(RNA.counts)
+
+# Establish column data
+RNA.condition <- meta[,3]
+colData <- as.data.frame(RNA.condition)
+row.names(colData) <- colnames(RNA.counts.df)
+
+# Run DEseq2
+RNA.dds <- DESeqDataSetFromMatrix(countData = RNA.counts.df, colData = colData, design = ~ RNA.condition)
+RNA.dds <- DESeq(RNA.dds, parallel = TRUE)
+
+# All pairwise comparisons
+comp <- expand.grid(paste0("P", seq(1:8)), paste0("P", seq(1:8)), stringsAsFactors = FALSE)
+comp <- subset(comp, Var1!=Var2)
+comp$cond <- "RNA.condition"
+comp <- comp %>% 
+  mutate(key = paste0(pmin(Var1, Var2), "_" ,pmax(Var1, Var2), sep = "")) %>%
+  dplyr::distinct(key, .keep_all=TRUE)
+
+# Function to do all pair-wise DEseq comparisons
+all.pairwise <- function(x) {
+  print(x)
+  # Do the DESeq contrast
+  resSig.RNA <- data.frame(results(RNA.dds, contrast=c(comp[x,3], comp[x,2], comp[x,1]), parallel = TRUE, filterFun=ihw, alpha = 0.01))
+  resSig.RNA$gene <- raw[,29]
+  resSig.RNA %>% filter(complete.cases(.)) %>% arrange(padj) %>% filter(padj < 0.01) -> out
+  
+  # Do some rounding
+  out$baseMean <- round( out$baseMean, 1)
+  out$log2FoldChange <- round( out$log2FoldChange, 1)
+  out$pvalue <-sprintf("%.3e", out$pvalue)
+  out$padj <-sprintf("%.3e", out$padj)
+   
+  # Output table
+  write.table(out[,c("gene", "baseMean", "log2FoldChange", "pvalue", "padj")],
+              file = paste0("../../downloads/RNA_DESeq2/RNAseq-", comp[x,4], "_Padj01.tsv", sep = "\t"), 
+              row.names = FALSE, col.names = TRUE, sep = "\t", quote = FALSE)
+  comp[x,4]
+  
+}
+dim(comp)
+dumb <- lapply(1:dim(comp)[1], all.pairwise)
