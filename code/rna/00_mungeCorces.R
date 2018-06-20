97670d48606a1d2601a5629724d87d25e4ba124b
caleblareau <caleblareau@g.harvard.edu>
2018-06-20 01:51:05 -0400

    various files reworked

--- code/rna/00_mungeCorces.R
+++ code/rna/00_mungeCorces.R
@@ -20,3 +20,13 @@ RNA.counts.all <- cbind(RNA.counts, df)
 celltype <- c(mapvec[colnames(RNA.counts)], stringr::str_split_fixed(colnames(df), "_", 4)[1:28,3])
 
 save(RNA.counts.all, celltype, file = "../../processed/serialized/RNAseq_wCorces.rda")
+
+cpm_ct <- sapply(unique(unname(celltype)), function(cell){
+  x <- rowSums(data.matrix(RNA.counts.all)[,cell == celltype], na.rm = TRUE)
+  x/sum(x) * 1000000
+})
+cpm_ct <- data.frame(round(cpm_ct, 2))
+cpm_ct$genes <- RNA.counts.all$genes
+
+write.table(cpm_ct[,c("HSC", "MPP", "CMP", "MEP", paste0("P", seq(1:8)), "genes")], 
+            file = "../../processed/expression_CPM_erythroid.tsv", sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
