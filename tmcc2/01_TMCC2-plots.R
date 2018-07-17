938013b2f5182bd83c02fc1d0d182eb33065d386
caleblareau <caleblareau@g.harvard.edu>
2018-07-17 18:52:56 -0400

    finished TMCC2 plots

--- tmcc2/01_TMCC2-plots.R
+++ tmcc2/01_TMCC2-plots.R
@@ -1,31 +1,67 @@
-library(data.table)
 library(dplyr)
 library(BuenColors)
 
-lapply(list.files("../../processed/all_DEseq2/"), function(x){
-  df <- read.table(paste0("../../processed/all_DEseq2/", x), header = TRUE)
-  df$timepoint <- stringr::str_split_fixed(x, "_", 3)[1,2]
-  df <- df[df$baseMean > 500,]
-  df[,c("gene", "log2FoldChange", "timepoint")]
-}) %>% rbindlist() %>% data.frame() -> allFC
+# 1) qPCR data
+qPCR_df <- data.frame(
+  shRNA = c(rep("shluc", 3), rep("sh1", 3),  rep("sh2", 3)),
+  val = c(1.29, 0.73, 1.06, 0.02, 0.15, 0.01, 0.03, 0.01, 0.01)
+)
 
+qPCR_df %>% group_by(shRNA) %>% summarise(mean = mean(val),
+                                          sd = sd(val)) %>% data.frame() -> qPCR_df_plot
+qPCR_df_plot$shRNA <- factor(as.character(qPCR_df_plot$shRNA), c("shluc", "sh1", "sh2"))
 
-lapply(list.files("../../downloads/RNA_DESeq2", full.names = TRUE), function(x){
-  data.frame(g = read.table(x, header = TRUE, stringsAsFactors = FALSE)[,1])
-}) %>% rbindlist() %>% data.frame() -> diffGeneDf
-differentialGenes <- unique(as.character(diffGeneDf[,1]))
-
-go <- allFC[as.character(allFC$gene) %in% differentialGenes, ]
-
-go <- rbind(go, data.frame(gene = unique(as.character(go$gene)), log2FoldChange = 0, timepoint = "P1"))
-go$interesting <- ifelse(go$gene == "TMCC2", "TMCC2", "zznone")
-go <- arrange(go, desc(interesting))
-go$gene <- factor(as.character(go$gene), levels = unique(as.character(go$gene)))
-
-p1 <- ggplot(go, aes(x = timepoint, y = log2FoldChange*-1, group = gene, color = interesting)) +
+# make plot one
+p1 <- ggplot(qPCR_df_plot, aes(x = shRNA, y = mean, fill = shRNA)) +
   pretty_plot(fontsize = 8) +
-  geom_line(size = 0.25) + scale_color_manual(values = c("TMCC2" = "firebrick", "zznone" = "grey")) + L_border() +
-  labs(x = "", y = "log2FC over P1") + theme(legend.position = "none")
+  geom_bar(stat="identity", color="black", 
+           position=position_dodge()) +
+  geom_errorbar(aes(ymin=mean, ymax=mean+sd), width=.2,
+                position=position_dodge(.9)) +
+  scale_fill_manual(values = c("shluc" = "black", "sh2" = "orange3", "sh1" = "firebrick")) + L_border() +
+  labs(x = "", y = "Relative Expression") + theme(legend.position = "none")
 
-cowplot::ggsave(p1, file = "../../plots/spaghetti.pdf", width = 3, height = 2)
+# 2) growth data
+growth_df <- data.frame(
+  shRNA = c(rep("shluc", 6), rep("sh1", 6),  rep("sh2", 6)),
+  day = rep(c(rep("D10", 2), rep("D12", 2), rep("D15", 2)),3),
+  val = c(1,1,1.47,1.42,2.23,2.26,1,1,1.02,0.94,1.21,0.95,1,1,1.06,1,1.04,0.94)
+)
+
+growth_df %>% group_by(shRNA,day) %>% summarise(mean = mean(val),
+                                          sd = sd(val)) %>% data.frame() -> growth_df_plot
+growth_df_plot$shRNA <- factor(as.character(growth_df_plot$shRNA), c("shluc", "sh1", "sh2"))
+
+# make plot one
+p2 <- ggplot(growth_df_plot, aes(x = day, y = mean, color = shRNA, group = shRNA)) +
+  pretty_plot(fontsize = 8) +
+  geom_line() +
+  geom_point() +
+  geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd), width=.1) +
+  scale_color_manual(values = c("shluc" = "black", "sh2" = "orange3", "sh1" = "firebrick")) + L_border() +
+  labs(x = "", y = "Fold Expansion") + theme(legend.position = "none") 
+
+# 3) qPCR data
+ann_df <- data.frame(
+  shRNA = c(rep("shluc", 2), rep("sh1", 2),  rep("sh2", 2)),
+  val = c(4.9,4.5,17.15,17.24,18.64,19)
+)
+
+ann_df %>% group_by(shRNA) %>% summarise(mean = mean(val),
+                                          sd = sd(val)) %>% data.frame() -> ann_df_plot
+ann_df_plot$shRNA <- factor(as.character(ann_df_plot$shRNA), c("shluc", "sh1", "sh2"))
+
+# make plot one
+p3 <- ggplot(ann_df_plot, aes(x = shRNA, y = mean, fill = shRNA)) +
+  pretty_plot(fontsize = 8) +
+  geom_bar(stat="identity", color="black", 
+           position=position_dodge()) +
+  geom_errorbar(aes(ymin=mean, ymax=mean+sd), width=.2,
+                position=position_dodge(.9)) +
+  scale_fill_manual(values = c("shluc" = "black", "sh2" = "orange3", "sh1" = "firebrick")) + L_border() +
+  labs(x = "", y = "AnnexinV+ %") + theme(legend.position = "none")
+
+
+cowplot::ggsave(cowplot::plot_grid(p1, p2, p3, nrow = 1),
+                file = "threePanels.pdf", width = 6.3, height = 2)
 
