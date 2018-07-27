532b1d301395a8357720053d9e409293c0332fd6
Erik <ebao@broadinstitute.org>
2018-07-27 11:48:08 -0400

    gchromVAR ery only

--- code/atac/03_gChromVAR.R
+++ code/atac/03_gChromVAR.R
@@ -43,3 +43,41 @@ ggplot(mdf[mdf$Var1 %in% c( "HGB", "MCV", "MCH", "MCHC", "RETIC_COUNT",
 
 ggsave(p1, filename = "../../plots/gchromVAR_selected.pdf", width = 3.5, height = 1.5)
 
+
+# g-chromVAR using P1-P8 only --------------------------------------------------------------
+
+# Import and run run run 
+peaksdf <- fread("../../data/ATAC_data/ery_only.bed")
+peaks <- makeGRangesFromDataFrame(peaksdf, seqnames = "V1", start.field = "V2", end.field = "V3")
+
+ATAC.counts <- data.matrix(data.frame(data.table::fread("../../data/ATAC_data/ery_only.counts.tsv")))
+meta <- stringr::str_split_fixed(colnames(ATAC.counts), "_", 4)
+pops.ordered <- c( paste0("P", as.character(1:8)))
+
+sapply(pops.ordered, function(pop){
+  rowSums(ATAC.counts[,meta[,3] == pop])
+}) -> counts
+
+# Create objects for g-chromVAR
+SE <- SummarizedExperiment(assays = list(counts = counts),
+                           rowData = peaks, 
+                           colData = DataFrame(names = colnames(counts)))
+SE <- filterPeaks(SE)
+SE <- addGCBias(SE, genome = BSgenome.Hsapiens.UCSC.hg19)
+ukbb <- importBedScore(rowRanges(SE), list.files("../../data/FMsnps/", full.names = TRUE, pattern = "*.bed$"))
+
+# Run g-chromVAR
+bg <- getBackgroundPeaks(SE)
+dev <- computeWeightedDeviations(SE, ukbb, background_peaks = bg)
+outdf <- assays(dev)[["z"]]
+rownames(outdf) <- gsub("_PP001_betas", "", rownames(outdf))
+mdf <- reshape2::melt(outdf)
+mdf$Var2 <- factor(as.character(mdf$Var2), levels = c("HSC", "MPP", "CMP", "MEP", paste0("P", as.character(1:8))))
+
+p2 <- ggplot(mdf[mdf$Var1 %in% c("HGB","HCT","RBC_COUNT",
+                           "MCHC","MCV","MCH","RETIC_COUNT","MEAN_RETIC_COUNT") , ],
+       aes(x = Var2, y = value, color = Var1, group = Var1)) +
+  geom_point() + geom_line() + pretty_plot(fontsize = 8) + L_border() + 
+  labs(x = "", y = "g-chromVAR Zscore", color = "") + theme(legend.position = "right") 
+
+ggsave(p2, filename = "../../plots/gchromVAR_eryonly.pdf", width = 3.5, height = 1.5)
